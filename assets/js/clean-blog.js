6b537c1a19a8f57bb37938fc4ffe821aef7f31d3
fabmoll <fabrice.moll@hotmail.com>
2021-07-10 20:27:55 +0000

    Update.

--- assets/js/clean-blog.js
+++ assets/js/clean-blog.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
                 } else {
                     //if scrolling down...
                     $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                    // if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
                 }
                 this.previousTop = currentTop;
             });
